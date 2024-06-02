b575c8fe1cb4b8cc2951cd676cb9b211cca0611f
Sugandha <shakyasugandha@gmail.com>
2024-06-02 18:49:27 +0545

    set cors origin policy

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -19,5 +19,13 @@ module Backend
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :async
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
