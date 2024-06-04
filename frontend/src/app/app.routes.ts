f26a0dd333ce786619fae813e0e6fc49d1551dad
Sugandha <shakyasugandha@gmail.com>
2024-06-04 17:37:48 +0545

    removing unecessary nav

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { ScrapeDataComponent } from './scrape-data/scrape-data.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // { path:'', component: HomeComponent},
     { path:'webScraper', component: ScrapeDataComponent},
     { path:'products', component: ProductsComponent},
     
